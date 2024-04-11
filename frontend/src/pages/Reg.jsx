931ef2bdde0fb293d4f8b57dc85ee4ed4824ef59
Soopec42 <romansaushkin99@gmail.com>
2024-04-11 20:33:13 +0500

    test

--- frontend/src/pages/Reg.jsx
+++ frontend/src/pages/Reg.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios'
-import Mybutton from "../components/UI/button/Mybutton";
-import MyInput from "../components/UI/input/MyInput";
+
+
 
 function Reg() {
     const [username, setUsername] = useState('');
@@ -11,6 +11,8 @@ function Reg() {
     const [name_3, setName_3] = useState('');
 
     const [users, setUsers] = useState([]);
+
+
 
     useEffect(() => {
         async function fetchUsers() {
@@ -33,7 +35,7 @@ function Reg() {
                 password: password,
                 name_1: name_1,
                 name_2: name_2,
-                name_3: name_3
+                name_3: name_3,
             });
             console.log(response.data);
         } catch (error) {
@@ -41,21 +43,66 @@ function Reg() {
         }
     };
 
+
+
     return (
-        <div className="FormReg">
-            <div className="Login">
-                <h2 style={{width: "400px", margin: "0 0 15px 0"}}>Регистрация</h2>
-                <form onSubmit={handleSubmit}>
-                    <MyInput type="text" name="Логин" value={username} onChange={(e) => setUsername(e.target.value)}></MyInput>
-                    <MyInput name="Пароль" type="password" value={password} onChange={(e) => setPassword(e.target.value)}></MyInput>
-                    <MyInput type="text" name="Имя" value={name_1} onChange={(e) => setName_1(e.target.value)}></MyInput>
-                    <MyInput type="text" name="Фамилия" value={name_2} onChange={(e) => setName_2(e.target.value)}></MyInput>
-                    <MyInput type="text" name="Отчество" value={name_3} onChange={(e) => setName_3(e.target.value)}></MyInput>
-                    <Mybutton style={{width: "200px", height: "50px"}} >Создать аккаунт</Mybutton>
-                </form>
+        <div className="main">
+            <div className="FormReg">
+                <div className="Login">
+                    <h2 style={{width: "400px", margin: "0 0 15px 0"}}>Регистрация</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className="DivInp">
+                            <p className="NameDiv">Логин</p>
+                            <input
+                                className="Input"
+                                type="text"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                        </div>
+                        <div className="DivInp">
+                            <p className="NameDiv">Пароль</p>
+                            <input
+                                className="Input"
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <div className="DivInp">
+                            <p className="NameDiv">Имя</p>
+                            <input
+                                className="Input"
+                                type="text"
+                                value={name_1}
+                                onChange={(e) => setName_1(e.target.value)}
+                            />
+                        </div>
+                        <div className="DivInp">
+                            <p className="NameDiv">Фамилия</p>
+                            <input
+                                className="Input"
+                                type="text"
+                                value={name_2}
+                                onChange={(e) => setName_2(e.target.value)}
+                            />
+                        </div>
+                        <div className="DivInp">
+                            <p className="NameDiv">Отчество</p>
+                            <input
+                                className="Input"
+                                type="text"
+                                value={name_3}
+                                onChange={(e) => setName_3(e.target.value)}
+                            />
+                        </div>
+                        <button id="regBtn" className="Btn" type="submit">Создать аккаунт</button>
+                    </form>
+                </div>
             </div>
         </div>
     );
 }
+
 
 export default Reg;
